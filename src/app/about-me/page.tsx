ae47d7d4ea172197ed0c58fd03173c1b025faf25
Arcticsun1 <160473700+Arcticsun1@users.noreply.github.com>
2024-10-06 16:10:16 +0800

    about me page

---
+++ src/app/about-me/page.tsx
@@ -0,0 +1,49 @@
+
+import Image from "next/image";
+import photo from "@/assets/camphoto_684387517.jpg";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+export default function AboutMe() {
+  return (
+    <main>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/Info">Info</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>About-me</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+        <h1 className = "px-5">
+        <h2 className="text-3xl font-bold text-[#000000]">About me</h2>
+      <Image
+        src = {photo}
+        alt = "test image"
+        width = {300}
+        height = {300}
+      />
+      <div className= "flex gap-2"> <p className= "font-bold" >Name :</p> Dylan</div>
+      <div className= "flex gap-2"> <p className= "font-bold" >Major :</p> CS (Y2)</div>
+      <div className= "flex gap-2"> <p className= "font-bold" >Why Heartcode:</p> the lols</div>
+        </h1>
+
+
+    </main>
+  );
+}
